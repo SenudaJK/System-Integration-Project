11a388516565125ed514742c5deb0ac5170ee18e
Manditha77 <manditha77madushanka@gmail.com>
2025-05-23 23:55:09 +0530

    Added a new method to FuelStationRepository

--- src/main/java/com/fuelquota/management/repository/FuelStationRepository.java
+++ src/main/java/com/fuelquota/management/repository/FuelStationRepository.java
@@ -4,6 +4,9 @@ import com.fuelquota.management.model.FuelStation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FuelStationRepository extends JpaRepository<FuelStation, Long> {
+    Optional<FuelStation> findByContactNumber(String contactNumber);
 }
