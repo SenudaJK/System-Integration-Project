0c56c353a37d2ba1f38fdcce90650e598055fbc3
Manditha77 <manditha77madushanka@gmail.com>
2025-05-24 00:13:31 +0530

    Now controller call FuelStationService(to register, getall and get by id)

--- src/main/java/com/fuelquota/management/controller/FuelStationController.java
+++ src/main/java/com/fuelquota/management/controller/FuelStationController.java
@@ -1,4 +1,43 @@
 package com.fuelquota.management.controller;
 
+import com.fuelquota.management.dto.FuelStationDTO;
+import com.fuelquota.management.service.FuelStationService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/fuel-stations")
 public class FuelStationController {
+
+    private final FuelStationService fuelStationService;
+
+    @Autowired
+    public FuelStationController(FuelStationService fuelStationService) {
+        this.fuelStationService = fuelStationService;
+    }
+
+    @PostMapping
+    public ResponseEntity<FuelStationDTO> registerFuelStation(@Valid @RequestBody FuelStationDTO fuelStationDTO) {
+        FuelStationDTO createdFuelStation = fuelStationService.registerFuelStation(fuelStationDTO);
+        return new ResponseEntity<>(createdFuelStation, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<FuelStationDTO> getFuelStationById(@PathVariable Long id) {
+        FuelStationDTO fuelStationDTO = fuelStationService.getFuelStationById(id);
+        return ResponseEntity.ok(fuelStationDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<FuelStationDTO>> getAllFuelStations() {
+        List<FuelStationDTO> fuelStations = fuelStationService.getAllFuelStations();
+        return ResponseEntity.ok(fuelStations);
+    }
+
+
 }
