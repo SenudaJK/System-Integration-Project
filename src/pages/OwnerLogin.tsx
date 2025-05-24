cc20ec11aba06f98fe6fba82f01e90bc6dc8a597
Manditha77 <manditha77madushanka@gmail.com>
2025-05-24 18:58:40 +0530

    Updated OwnerLogin.tsx

--- src/pages/OwnerLogin.tsx
+++ src/pages/OwnerLogin.tsx
@@ -74,7 +74,7 @@ const OwnerLogin = () => {
                     </button>
                 </form>
                 <p className="mt-4 text-center">
-                    Don't have an account? <a href="/" className="text-blue-500 hover:underline">Register here</a>
+                    Don't have an account? <a href="/register" className="text-blue-500 hover:underline">Register here</a>
                 </p>
             </div>
         </div>
