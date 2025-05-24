c8af5d845082168f0aa5cd97e5ac9e270112719a
Manditha77 <manditha77madushanka@gmail.com>
2025-05-24 18:55:56 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import OwnerRegistration from './pages/OwnerRegistration';
 import './App.css';
+import OrderPage from "./pages/OrderPage.tsx";
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<OwnerRegistration />} />
+                <Route path="/orders" element={<OrderPage />} />
             </Routes>
         </Router>
     );
