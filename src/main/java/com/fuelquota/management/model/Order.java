8e1535e50e3d2c1d2af559d0792fb854b483ec49
hasardulmi <hasardulmi@gmail.com>
2025-05-24 18:42:48 +0530

    Order.java code is added

---
+++ src/main/java/com/fuelquota/management/model/Order.java
@@ -0,0 +1,24 @@
+package com.fuelquota.management.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "orders") // Explicitly set table name to avoid reserved keyword
+@Data
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+
+    @Column(nullable = false)
+    private LocalDate orderDate;
+
+    @Column(nullable = false)
+    private Double orderAmount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private FuelType fuelType;
+}
